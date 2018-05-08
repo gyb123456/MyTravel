84bfa5d961339a658a40647a79be35b2b91a5e08
gyb <578498005@qq.com>
2018-05-08 15:36:03 +0800

    second commit

---
+++ src/pages/list/List.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="home">List</div>
+  </div>
+</template>
+
+<script>
+export default {
+ name:'List1'
+}
+</script>
+
+<style>
+    .home{
+        font-size: 50px;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Home from '@/pages/home/Home'
+import List from '@/pages/list/List'
 Vue.use(Router)
+
+// const List = { template: '<div><h2>我是list页面</h2></div>' }
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: List
     }
   ]
 })
