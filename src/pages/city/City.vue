639863b4d54f0bcfbafb41ae6e634355fce71cda
gyb <578498005@qq.com>
2018-05-23 18:33:50 +0800

    gyb

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <city-header></city-header>
+        <city-search></city-search>
+        
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
-
+import CitySearch from './components/Search'
 
 export default {
     components:{
-        CityHeader:CityHeader
+        CityHeader:CityHeader,
+        CitySearch
     }
 }
 </script>
