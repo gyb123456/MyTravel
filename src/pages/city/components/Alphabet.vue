16583910245b904ec5be66c2526c0ec117954245
gyb <578498005@qq.com>
2018-05-24 14:54:50 +0800

    gyb20180524-15:00

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <ul class="list">
+            <li class="item">A</li>
+            <li class="item">A</li>
+            <li class="item">A</li>
+            <li class="item">A</li>
+            <li class="item">A</li>
+            <li class="item">A</li>
+            <li class="item">A</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl'
+    .list
+        position absolute
+        right 0
+        top 6rem
+        line-height .4rem
+        color $bgColor
+        // background red
+        display flex
+        flex-direction column
+        width .4rem
+        text-align center
+
+
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="list" ref="wrapper">
+        <div>
+         <div class="area">
+             <div class="title border-topbottom">当前城市</div>
+             <div class="btn-list">
+                 <div class="btn-wrapper">
+                     <div class="btn">北京</div>
+                 </div>
+             </div>
+         </div>
+         <div class="area">
+             <div class="title border-topbottom">热门城市</div>
+             <div class="btn-list">
+                 <div class="btn-wrapper">
+                     <div class="btn">北京</div>
+                 </div>
+                 <div class="btn-wrapper">
+                     <div class="btn">北京</div>
+                 </div>
+                 <div class="btn-wrapper">
+                     <div class="btn">北京</div>
+                 </div>
+                 <div class="btn-wrapper">
+                     <div class="btn">北京</div>
+                 </div>
+                 <div class="btn-wrapper">
+                     <div class="btn">北京</div>
+                 </div>
+                 <div class="btn-wrapper">
+                     <div class="btn">北京</div>
+                 </div>
+                 <div class="btn-wrapper">
+                     <div class="btn">北京</div>
+                 </div>
+             </div>
+         </div>
+         <div class="area">
+             <div class="title border-topbottom">A</div>
+             <div class="item-list">
+                <div class="item border-topbottom">阿拉尔</div>
+                <div class="item border-topbottom">阿拉尔</div>
+                <div class="item border-topbottom">阿拉尔</div>
+                <div class="item border-topbottom">阿拉尔</div>
+                <div class="item border-topbottom">阿拉尔</div>
+                <div class="item border-topbottom">阿拉尔</div>
+             </div>
+             <div class="title border-topbottom">B</div>
+             <div class="item-list">
+                <div class="item border-topbottom">阿拉尔</div>
+                <div class="item border-topbottom">阿拉尔</div>
+                <div class="item border-topbottom">阿拉尔</div>
+                <div class="item border-topbottom">阿拉尔</div>
+                <div class="item border-topbottom">阿拉尔</div>
+                <div class="item border-topbottom">阿拉尔</div>
+             </div>
+         </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Bscroll from "better-scroll"
+
+export default {
+    mounted(){
+        this.scroll = new Bscroll(this.$refs.wrapper, {})
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+ .title
+    line-height .54rem
+    background #eee
+    padding-left .2rem
+    color #669
+ .border-topbottom
+    &:before
+        border-color #777
+    // &:after
+    //     border-color #777   
+ .list
+    position absolute
+    overflow hidden
+    // background red
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    .btn-list
+        padding .1rem .6rem .1rem .2rem
+        overflow hidden
+        .btn-wrapper
+            float left
+            width 33.3%
+            // background #0f0
+            .btn
+                text-align center
+                border .02rem solid 
+                padding .1rem 
+                margin .1rem .1rem
+                // background #0f0
+                border-radius .06rem
+    .item-list
+        .item
+            line-height .7rem
+            color #666
+            padding-left .2rem
+//  .test
+//     position relative
+//     overflow scroll
+//     // background green
+//     width 100%
+</style>
